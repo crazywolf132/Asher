a5b8b7d124678b5ce30347a9234fc850b73626c8
crazywolf132 <crazywolf132@gmail.com>
2016-07-11 21:00:49 +1000

    Removed unneeded shit.

--- tests/conversation_tests/test_responses.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from samantha.conversation import Response
-
-
-class ResponseTests(TestCase):
-
-    def setUp(self):
-        self.response = Response("A test response.")
-
--- tests/output_adapter_tests/test_mailgun_adapter.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from samantha.adapters.output import Mailgun
-
-
-class MailgunAdapterTestCase(TestCase):
-
-    def setUp(self):
-        super(MailgunAdapterTestCase, self).setUp()
-        self.adapter = Mailgun()
