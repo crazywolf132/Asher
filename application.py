8bec83a654abcb9ba610f5f0a34c2230743378e7
Crazywolf132 <crazywolf132@gmail.com>
2016-07-12 13:02:17 +1000

    Updated AI

--- application.py
+++ application.py
@@ -1,27 +1,26 @@
 from samantha import Samantha
 
-class run():
-   def startmain(self):
-      # Create a new instance of Samantha
-      bot = Samantha("Samantha",
-          storage_adapter="samantha.adapters.storage.JsonDatabaseAdapter",
-            logic_adapters=[
-                "samantha.adapters.logic.ClosestMatchAdapter"
-            ],
-            input_adapter="samantha.adapters.input.TerminalAdapter",
-            output_adapter="samantha.adapters.output.TerminalAdapter",
-            database="./database.db"
-      )
 
-      print("Type something to begin...")
+# Create a new instance of a ChatBot
+bot = Samantha("No Output",
+        storage_adapter="samantha.adapters.storage.JsonDatabaseAdapter",
+        logic_adapters=[
+            "samantha.adapters.logic.ClosestMatchAdapter"
+        ],
+        input_adapter="samantha.adapters.input.TerminalAdapter",
+        output_adapter="samantha.adapters.output.TerminalAdapter",
+        database="../database.db"
+    )
 
-      # The following loop will execute each time the user enters input
-      while True:
-          try:
-              # We pass None to this method because the parameter
-              # is not used by the TerminalAdapter
-              bot_input = bot.get_response(None)
+print("Type something to begin...")
 
-          # Press ctrl-c or ctrl-d on the keyboard to exit
-          except (KeyboardInterrupt, EOFError, SystemExit):
-              break
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        # We pass None to this method because the parameter
+        # is not used by the TerminalAdapter
+        bot_input = bot.get_response(None)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
