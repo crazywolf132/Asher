b0bea83c673218aefe2ad15b7e95f15c75a56b02
crazywolf132 <crazywolf132@gmail.com>
2016-07-26 14:54:13 +1000

    Testing the updater

--- samantha/plugins/test_plugin.py
+++ samantha/plugins/test_plugin.py
@@ -18,12 +18,18 @@ class Plugin:
     def action(self, command, statement, sam):
         sam_print(sam, 'Please dont think that this file is going to work.')
         system('clear')
-        sam_print(sam, 'Going to run an update now.')
-        sleep(0.2)
-        system('cd ~/')
-        system('curl -L "https://raw.githubusercontent.com/crazywolf132/Ai-Setup/master/Update.sh" > update.sh')
-        system('bash update.sh')
-        sleep(3)
-        system('clear')
-        sam_print(sam, 'Going to restart system apply all updates.')
-        execv('/usr/bin/env', ('env', 'python', application, 'All done.'))
+        import urllib
+        try :
+            stri = "https://8.8.8.8/"
+            data = urllib.urlopen(stri)
+            sam_print(sam, 'Going to run an update now.')
+            sleep(0.2)
+            system('cd ~/')
+            system('curl -L "https://raw.githubusercontent.com/crazywolf132/Ai-Setup/72f29e63108f40f052e95a1f87f7f94efef023a2/Update.sh" > update.sh')
+            system('bash update.sh')
+            sleep(3)
+            system('clear')
+            sam_print(sam, 'Going to restart system apply all updates.')
+            execv('/usr/bin/env', ('env', 'python', application, 'All done.'))
+        except e:
+            sam_print(sam, 'Please connect to the internet to download any updates.')
