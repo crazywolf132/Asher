9c88f32af2b6e64764311656be51c30def23cfbf
crazywolf132 <crazywolf132@gmail.com>
2016-07-26 10:46:06 +1000

    Funky colors!

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
 from samantha import Samantha
 from sys import argv
+import random
 
+messages = ['\033[1;31m',
+            '\033[1;32m',
+            '\033[1;33m',
+            '\033[1;34m',
+            '\033[1;35m',
+            '\033[1;36m']
+message = random.choice(messages)
 
 # Create a new instance of the AI
 bot = Samantha("No Output",
@@ -14,7 +22,7 @@ bot = Samantha("No Output",
     )
 
 
-print("Type something to begin...")
+print(message + "Type something to begin...\033[0m")
 
 if len(argv) >= 2:
     print argv[1]
